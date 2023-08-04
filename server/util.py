2e57b9a34c681f18afd0e1ffe00ca22535d1d201
Aashish <ashishkuchhal68@gmail.com>
2023-08-04 11:55:01 +0530

    04/08/23

---
+++ server/util.py
@@ -0,0 +1,14 @@
+import cv2
+def classify_image(image_base64_data,filae_path=None):
+    pass
+
+def get_cropped_image_if_2_eye(image_path,image_bs64_data):
+    face_cascade=cv2.CascadeClassifier('/content/drive/MyDrive/image classifier/haarcascade_frontalface_default.xml')
+    eye_cascade=cv2.CascadeClassifier('/content/drive/MyDrive/image classifier/haarcascade_eye.xml')
+
+def get_bs6_image_test_virat():
+    with open('bs6.txt') as f:
+        return f.read()
+
+if __name__=="__main__":
+    
