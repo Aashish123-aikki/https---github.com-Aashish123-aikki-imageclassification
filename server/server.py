2d8b932d795ac7a16d0a1b7a29436978dee1508f
Aashish <ashishkuchhal68@gmail.com>
2023-08-06 10:21:19 +0530

    06-08-2023

--- server/server.py
+++ server/server.py
@@ -1,10 +1,16 @@
 from flask import Flask,request,jsonify
-
+import util
 app=Flask(__name__)
 
 @app.route('/classify_image',methods=['GET','POST'])
 def classify_image():
-    return "ok"
+    image_data=request.form['image_data']
+    response=jsonify(util.classify_image(image_data))
+    response.headers.add('Access-Control-Allow-Origin','*')
+
+    return response
 
 if __name__=="__main__":
-    app.run(port=5001)
+    print('starting python flask server')
+    util.load_artifacts()
+    app.run(port=5000)
